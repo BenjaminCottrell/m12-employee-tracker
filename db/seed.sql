60741d747f8989c17d34cade7f66fc539d9ade71
BenCottrell <bencottrell48@yahoo.com>
2022-12-14 21:44:49 -0500

    fixed typos in seeds

--- db/seed.sql
+++ db/seed.sql
@@ -8,11 +8,11 @@ VALUES  ('Marketing Director', 95000, 1),
         ('Marketing Manager', 50000, 1),
         ('Senior Engineer', 85000, 2),
         ('Junior Engineer', 65000, 2),
-        ('HR Manager', 55000, 3) ;
+        ('HR Manager', 55000, 3);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Boba, Fett', 1, NULL),
+VALUES  ('Boba', 'Fett', 1, NULL),
         ('Han', 'Solo', 2, 1),
         ('Luke', 'Skywalker', 2, NULL),
         ('Leia', 'Organa', 4, 3),
