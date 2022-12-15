7029123f252dcf1e25eebf830a4b5fea8e24b045
BenCottrell <bencottrell48@yahoo.com>
2022-12-14 20:01:01 -0500

    added seeds

---
+++ db/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES  ('Marketing'),
+        ('Engineering'),
+        ('Human Resources');
+
+INSERT INTO roles (title, salary)
+VALUES  ('Marketing Director', 95000),
+        ('Senior Engineer', 85000)
+        ('Junior Engineer', 65000),
+        ('HR Manager', 55000);
+
+
+INSERT INTO employee (first_name, last_name)
+VALUES  ('Boba, Fett'),
+        ('Han', 'Solo'),
+        ('Luke', 'Skywalker'),
+        ('Darth', 'Vader');
